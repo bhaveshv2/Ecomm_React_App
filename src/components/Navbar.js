a8e9071bdbe932acdf3c27fd125623f5016a7fa4
bhaveshv2 <bhaveshv3024@gmail.com>
2020-09-09 17:04:58 +0530

    Final Tweaks Done.: Final Commit...

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ class Navbar extends Component {
               {/* Company logo */}
               <div className="logo-container">
                 <Link to="/" >
-                  <img src="https://png2.cleanpng.com/sh/507d81c088fc05468d71f6f336c95a91/L0KzQYm3WcI3N5D0kpH0aYP2gLBuTfNwdaF6jNd7LXnmf7B6TgNkaZ1met5uLYboc8X2kr1oepJ1gNtscz3kgMHzifNifJp0ReZEbnB1Pcj5igN1NaR1hNt3dD3kfbPwhPV5fKN0jeU2coOwQoi7TgdzcaR5ReU2NXLkc4S4VPEzamQ9Sqc3NkG6RoqAVMIyPWQ9SKM6NEa5QYiATwBvbz==/kisspng-computer-icons-scalable-vector-graphics-applicatio-tynor-wrist-splint-ambidextrous-rs-274-wrist-s-5bac314a2b3825.617697421538011466177.png" alt="logo" />
+                  <img src="https://image.flaticon.com/icons/svg/777/777205.svg" alt="logo" />
                 </Link>
               </div>
             </div>
